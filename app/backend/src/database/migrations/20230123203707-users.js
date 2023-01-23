f28b89dba288f143cc30675a65855c8f27b55978
Pedro Luiz <pedroluizlsg@gmail.com>
2023-01-23 19:49:05 -0300

    fix: reorganiza identações nas migrations

--- app/backend/src/database/migrations/20230123203707-users.js
+++ app/backend/src/database/migrations/20230123203707-users.js
@@ -2,32 +2,32 @@
 
 module.exports = {
 up: async (queryInterface, Sequelize) => {
-await queryInterface.createTable('users', {
-id: {
-type: Sequelize.INTEGER,
-primaryKey: true,
-allowNull: false,
-autoIncrement: true,
-},
-username: {
-allowNull: false,
-type: Sequelize.STRING,
-},
-role: {
-allowNull: false,
-type: Sequelize.STRING,
-},
-email: {
-allowNull: false,
-type: Sequelize.STRING,
-},
-password: {
-allowNull: false,
-type: Sequelize.STRING,
-}
-});
-},
-down: async (queryInterface) => {
-await queryInterface.dropTable('users');
-},
+  await queryInterface.createTable('users', {
+      id: {
+      type: Sequelize.INTEGER,
+      primaryKey: true,
+      allowNull: false,
+      autoIncrement: true,
+      },
+      username: {
+      allowNull: false,
+      type: Sequelize.STRING,
+      },
+      role: {
+      allowNull: false,
+      type: Sequelize.STRING,
+      },
+      email: {
+      allowNull: false,
+      type: Sequelize.STRING,
+      },
+      password: {
+      allowNull: false,
+      type: Sequelize.STRING,
+      }
+    });
+  },
+  down: async (queryInterface) => {
+  await queryInterface.dropTable('users');
+  },
 }; 
--- app/backend/src/database/migrations/20230123203715-teams.js
+++ app/backend/src/database/migrations/20230123203715-teams.js
@@ -2,20 +2,20 @@
 
 module.exports = {
 up: async (queryInterface, Sequelize) => {
-await queryInterface.createTable('teams', {
-id: {
-type: Sequelize.INTEGER,
-allowNull: false,
-primaryKey: true,
-autoIncrement: true,
-},
-team_name: {
-type: Sequelize.STRING,
-allowNull: false,
-}
-});
-},
-down: async (queryInterface) => {
-await queryInterface.dropTable('teams');
-},
+  await queryInterface.createTable('teams', {
+    id: {
+      type: Sequelize.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true,
+      },
+      team_name: {
+      type: Sequelize.STRING,
+      allowNull: false,
+      }
+    });
+  },
+  down: async (queryInterface) => {
+  await queryInterface.dropTable('teams');
+  },
 }; 
